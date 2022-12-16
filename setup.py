ce8a4cad13bfab90a136c07bfdb1119ebb1f619f
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2022-12-16 17:33:55 -0500

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
 
 setup(
     name             = 'orthanc_push',
-    version          = '1.0.1',
+    version          = '1.0.2',
     description      = 'An app to push/upload dicoms to an orthanc server',
     long_description = readme,
     author           = 'FNNDSC',
