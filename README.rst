9a6176b2fdd127e89462ba3176e719aaa105bf03
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2023-02-02 17:48:48 -0500

    Add opinionated logging, minor README updates, and exec time and bump to 1.0.4

--- README.rst
+++ README.rst
@@ -17,8 +17,7 @@ pl-orthanc_push
 Abstract
 --------
 
-An app to push/upload dicoms to an orthanc server and to a remote modality
-(if specified)
+An app to push/upload dicoms to an orthanc server and optionally also "piggy back" to a remote modality (if specified).
 
 
 Description
@@ -26,7 +25,7 @@ Description
 
 
 ``orthanc_push`` is a *ChRIS ds-type* application that takes in DICOMs as input files
-and pushes them to a remote Orthanc server and to an optional remote modality
+and pushes them to a remote Orthanc server and in addition also to an optional remote modality.
 
 
 Usage
@@ -35,11 +34,11 @@ Usage
 .. code::
 
     docker run --rm fnndsc/pl-orthanc_push orthanc_push
-        [-f|--inputFileFilter <inputFileFilter>]                    
-        [-o|--orthancUrl <orthancServerUrl>]                       
-        [-u|--username <orthancUserName>]                          
-        [-p|--password <orthancPassword>]                         
-        [-r|--pushToRemote <remoteModality>]                     
+        [-f|--inputFileFilter <inputFileFilter>]
+        [-o|--orthancUrl <orthancServerUrl>]
+        [-u|--username <orthancUserName>]
+        [-p|--password <orthancPassword>]
+        [-r|--pushToRemote <remoteModality>]
         [-h|--help]
         [--json] [--man] [--meta]
         [--savejson <DIR>]
@@ -56,39 +55,39 @@ Arguments
     [-f|--inputFileFilter <inputFileFilter>]
     A glob pattern string, default is "**/*.dcm", representing the input
     file pattern to analyze.
-        
+
     [-o|--orthancUrl <orthancServerUrl>]
     URL of the orthanc server.
-        
+
     [-u|--username <orthancUserName>]
     The username to login to the orthanc server.
-        
+
     [-p|--password <orthancPassword>]
-    Specify the password to login to the orthanc server. 
-        
+    Specify the password to login to the orthanc server.
+
     [-r|--pushToRemote <remoteModality>]
-    If specified, orthanc will send dicoms to the target remote modality       
+    If specified, orthanc will send dicoms to the target remote modality
 
     [-h] [--help]
     If specified, show help message and exit.
-    
+
     [--json]
     If specified, show json representation of app and exit.
-    
+
     [--man]
     If specified, print (this) man page and exit.
 
     [--meta]
     If specified, print plugin meta data and exit.
-    
-    [--savejson <DIR>] 
-    If specified, save json representation file to DIR and exit. 
-    
+
+    [--savejson <DIR>]
+    If specified, save json representation file to DIR and exit.
+
     [-v <level>] [--verbosity <level>]
     Verbosity level for app. Not used currently.
-    
+
     [--version]
-    If specified, print version number and exit. 
+    If specified, print version number and exit.
 
 
 Getting inline help is:
@@ -105,9 +104,9 @@ You need to specify input and output directories using the `-v` flag to `docker 
 
 .. code:: bash
 
-    docker run --rm -u $(id -u)                             \
-        -v $(pwd)/in:/incoming -v $(pwd)/out:/outgoing      \
-        fnndsc/pl-orthanc_push orthanc_push                        \
+    docker run --rm -u $(id -u)                                 \
+        -v $(pwd)/in:/incoming -v $(pwd)/out:/outgoing          \
+        fnndsc/pl-orthanc_push orthanc_push                     \
         /incoming /outgoing
 
 
--- orthanc_push/orthanc_push.py
+++ orthanc_push/orthanc_push.py
@@ -14,16 +14,31 @@ import glob
 from pyorthanc import Orthanc, RemoteModality
 import logging
 import sys
+import  time
+from    loguru                  import logger
+LOG             = logger.debug
+
+logger_format = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> │ "
+    "<level>{level: <5}</level> │ "
+    "<yellow>{name: >28}</yellow>::"
+    "<cyan>{function: <30}</cyan> @"
+    "<cyan>{line: <4}</cyan> ║ "
+    "<level>{message}</level>"
+)
+logger.remove()
+logger.opt(colors = True)
+logger.add(sys.stderr, format=logger_format)
 
 Gstr_title = r"""
-            _   _                                          _     
-           | | | |                                        | |    
-  ___  _ __| |_| |__   __ _ _ __   ___     _ __  _   _ ___| |__  
- / _ \| '__| __| '_ \ / _` | '_ \ / __|   | '_ \| | | / __| '_ \ 
+            _   _                                          _
+           | | | |                                        | |
+  ___  _ __| |_| |__   __ _ _ __   ___     _ __  _   _ ___| |__
+ / _ \| '__| __| '_ \ / _` | '_ \ / __|   | '_ \| | | / __| '_ \
 | (_) | |  | |_| | | | (_| | | | | (__    | |_) | |_| \__ \ | | |
  \___/|_|   \__|_| |_|\__,_|_| |_|\___|   | .__/ \__,_|___/_| |_|
-                                    ______| |                    
-                                   |______|_|                    
+                                    ______| |
+                                   |______|_|
 """
 
 Gstr_synopsis = """
@@ -49,59 +64,61 @@ Gstr_synopsis = """
             [-v <level>] [--verbosity <level>]                          \\
             [--version]                                                 \\
             <inputDir>                                                  \\
-            <outputDir> 
+            <outputDir>
 
     BRIEF EXAMPLE
 
         * Bare bones execution
 
-            docker run --rm -u $(id -u)                             \
-                -v $(pwd)/in:/incoming -v $(pwd)/out:/outgoing      \
-                fnndsc/pl-orthanc_push orthanc_push                 \
+            docker run --rm -u $(id -u)                                 \\
+                -v $(pwd)/in:/incoming -v $(pwd)/out:/outgoing          \\
+                fnndsc/pl-orthanc_push orthanc_push                     \\
                 /incoming /outgoing
 
     DESCRIPTION
 
-        `orthanc_push` is an app to push/upload dicoms to an orthanc server
+        `orthanc_push` is an app to push/upload dicoms to an orthanc server.
+        It can also instruct that orthanc server to further push data to a
+        remote modality.
 
     ARGS
-    
+
         [-f|--inputFileFilter <inputFileFilter>]
         A glob pattern string, default is "**/*.dcm", representing the input
         file pattern to analyze.
-        
+
         [-o|--orthancUrl <orthancServerUrl>]
         URL of the orthanc server.
-        
+
         [-u|--username <orthancUserName>]
         The username to login to the orthanc server.
-        
+
         [-p|--password <orthancPassword>]
-        Specify the password to login to the orthanc server. 
-        
+        Specify the password to login to the orthanc server.
+
         [-r|--pushToRemote <remoteModality>]
-        If specified, orthanc will send dicoms to the target remote modality  
+        If specified, orthanc will send dicoms to the target remote modality
 
         [-h] [--help]
         If specified, show help message and exit.
-        
+
         [--json]
         If specified, show json representation of app and exit.
-        
+
         [--man]
         If specified, print (this) man page and exit.
 
         [--meta]
         If specified, print plugin meta data and exit.
-        
-        [--savejson <DIR>] 
-        If specified, save json representation file to DIR and exit. 
-        
+
+        [--savejson <DIR>]
+        If specified, save json representation file to DIR and exit.
+
         [-v <level>] [--verbosity <level>]
         Verbosity level for app. Not used currently.
-        
+
         [--version]
-        If specified, print version number and exit. 
+        If specified, print version number and exit.
 """
 
 
@@ -139,110 +156,122 @@ class Orthanc_push(ChrisApp):
         Define the CLI arguments accepted by this plugin app.
         Use self.add_argument to specify a new app argument.
         """
-        
+
         self.add_argument(  '--inputFileFilter','-f',
                             dest         = 'inputFileFilter',
                             type         = str,
                             optional     = True,
                             help         = 'Input file filter',
                             default      = '**/*.dcm')
-                            
+
         self.add_argument(  '--orthancUrl','-o',
                             dest         = 'orthancUrl',
                             type         = str,
                             optional     = True,
                             help         = 'Orthanc server url',
                             default      = 'http://0.0.0.0:8042')
-                            
+
         self.add_argument(  '--username','-u',
                             dest         = 'username',
                             type         = str,
                             optional     = True,
                             help         = 'Orthanc server url',
                             default      = 'orthanc')
-                            
+
         self.add_argument(  '--password','-p',
                             dest         = 'password',
                             type         = str,
                             optional     = True,
                             help         = 'Orthanc server url',
                             default      = 'orthanc')
-                            
+
         self.add_argument(  '--pushToRemote','-r',
                             dest         = 'pushToRemote',
                             type         = str,
                             optional     = True,
                             help         = 'Remote modality',
                             default      = '')
-                            
 
+    def preamble_show(self, options) -> None:
+        """
+        Just show some preamble "noise" in the output terminal
+        """
+
+        LOG(Gstr_title)
+        LOG('Version: %s' % self.get_version())
+
+        LOG("plugin arguments...")
+        for k,v in options.__dict__.items():
+             LOG("%25s:  [%s]" % (k, v))
+        LOG("")
+
+        LOG("base environment...")
+        for k,v in os.environ.items():
+             LOG("%25s:  [%s]" % (k, v))
+        LOG("")
 
     def run(self, options):
         """
         Define the code to be run by this plugin app.
         """
-        print(Gstr_title)
-        print('Version: %s' % self.get_version())
-        
-        
-        # Output the space of CLI
-        d_options = vars(options)
-        for k,v in d_options.items():
-            print("%20s: %-40s" % (k, v))
-        print("")
-        
-        # Create a logger object
-        logger = logging.getLogger()
-            
-        # set level 
-        logger.setLevel(logging.INFO)
-        
-        # add handler
-        logger.addHandler(logging.StreamHandler(stream=sys.stdout))
-        
+        st: float = time.time()
+        self.preamble_show(options)
+
+        # # Create a logger object
+        # logger = logging.getLogger()
+
+        # # set level
+        # logger.setLevel(logging.INFO)
+
+        # # add handler
+        # logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+
         # lets create a log file in the o/p directory first
         log_file = os.path.join(options.outputdir,'progress.log')
-
         lf = open(log_file,"w")
-        
         # now create and configure logger
         file_handler = logging.FileHandler(log_file)
-        
         # add handler
         logger.addHandler(file_handler)
-        
+
         orthanc = Orthanc(options.orthancUrl,username=options.username,password=options.password)
         dcm_str_glob = '%s/%s' % (options.inputdir,options.inputFileFilter)
         l_dcm_datapath = glob.glob(dcm_str_glob, recursive=True)
-        
-        modality = RemoteModality(orthanc,options.pushToRemote) 
-        
+
+        modality = RemoteModality(orthanc,options.pushToRemote)
+
         data={}
         for dcm_datapath in l_dcm_datapath:
-            logger.info(f"Pushing dicom: {dcm_datapath} to orthanc \n")
+            LOG(f"Pushing dicom: {dcm_datapath} to orthanc \n")
             with open(dcm_datapath, 'rb') as file:
-            
+
                 try:
-                    data = orthanc.post_instances(file.read())                
+                    data = orthanc.post_instances(file.read())
                 except Exception as err:
-                    logger.info(f'{err} \n')
-                    
+                    LOG(f'{err} \n')
+
                 if len(options.pushToRemote)>0:
-                    resource_id = data['ID'] 
-                    logger.info(f'Pushing resource {resource_id} to {options.pushToRemote} \n')
-                    
-                    try:                   
+                    resource_id = data['ID']
+                    LOG(f'Pushing resource {resource_id} to {options.pushToRemote} \n')
+
+                    try:
                         response = modality.store( {'Resources':[resource_id]})
-                        logger.info(f'Response : {response}\n')
+                        LOG(f'Response : {response}\n')
                     except Exception as err:
-                        logger.info(f'{err} \n')
-                
-        # Close logging
-        logger.removeHandler(file_handler)
-        file_handler.close()
-        lf.close()
-        
-       
+                        LOG(f'{err} \n')
+
+        # # Close logging
+        # logger.removeHandler(file_handler)
+        # file_handler.close()
+        # lf.close()
+
+        LOG("Saving %s" % jsonFilePath)
+        with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+            jsonf.write(json.dumps(d_json, indent=4))
+        et: float = time.time()
+        LOG("Execution time: %f seconds." % (et -st))
+
+
 
     def show_man_page(self):
         """
