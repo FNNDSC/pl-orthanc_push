cff1099802986c911582877a03ba70f366378bc3
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2023-03-24 13:49:03 -0400

    Bump to pull latest pflog

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
 
 setup(
     name             = 'orthanc_push',
-    version          = '1.2.0',
+    version          = '1.2.2',
     description      = 'An app to push/upload dicoms to an orthanc server',
     long_description = readme,
     author           = 'FNNDSC',
